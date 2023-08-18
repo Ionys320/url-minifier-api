9424c9890e8d12d58e9a1f93542c9e09695c3ff9
Ionys <9364594+Ionys320@users.noreply.github.com>
2023-08-18 21:53:43 +0200

    Handle custom port for the api

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+
+  const configService: ConfigService = app.get(ConfigService);
+  const port = configService.get<number>('port') || 3000;
+
+  await app.listen(port);
+
+  Logger.log(`URL minifier api on port ${port}`, 'Bootstrap');
 }
 bootstrap();
